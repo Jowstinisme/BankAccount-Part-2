812bc0ae4eb11a0b3861063d7a20fada446dd249
Jowstin <teamkiller1239@gmail.com>
2026-09-18 23:30:43 -0400

    feat: Overwritten the withdraw function so that the user is unable to withdraw an amount larger than the transferLimit.

--- checkingAccount.py
+++ checkingAccount.py
@@ -4,3 +4,10 @@ class checkingAccount(BankAccount):
     def __init__(self, name, balance, minBalance, accountNum, routingNum, transferLimit):
         super().__init__(name, balance, minBalance, accountNum, routingNum)
         self.transferLimit = transferLimit
+
+    def withdraw(self, amount):
+        if amount > self.transferLimit:
+            print(f"Withdraw failed, unable to withdraw more than {self.transferLimit}.")
+            return -1
+        else:
+            return super().withdraw(amount)
